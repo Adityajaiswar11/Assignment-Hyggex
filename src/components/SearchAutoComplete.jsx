13f7560025ff5f947bd6021b366ee5ee8d554b41
Aditya Jaiswar <adityajaiswar11@gmail.com>
2024-02-18 22:54:55 -0800

    Revise post api

--- src/components/SearchAutoComplete.jsx
+++ src/components/SearchAutoComplete.jsx
@@ -1,14 +1,62 @@
+import axios from 'axios';
 import { useState, useEffect } from "react";
 const SearchAutoComplete = () => {
   const [data, setData] = useState([]);
   const [filterdata, setFilterdata] = useState([]);
   const [inputvalue, setInputvalue] = useState("");
-  const url = `https://countriesnow.space/api/v0.1/countries`;
+
+  const [apiData,setApiData] = useState([])
+
+  
+    // Array of 10 objects
+   
+    // Iterate over the array of objects
+    useEffect(()=>{
+      const products = [
+        {
+          "id":21,
+          title: 'Iphone',
+          price: 20,
+          description: 'new Iphone',
+          image: 'https://i.pravatar.cc',
+          category: 'electronic'
+        },
+        // Add more objects here as needed
+        {
+          "id":22,
+          title: 'Product Title',
+          price: 30,
+          description: 'Product Description',
+          image: 'Product Image URL',
+          category: 'Product Category'
+        },
+        // Repeat for each additional object
+      ];
+    
+      products.forEach(product => {
+        axios.post('https://fakestoreapi.com/products', product)
+          .then((data) => {
+         
+            // Handle successful response
+            setApiData([data.data,...apiData]); // Assuming setApiData is updating an array state
+          })
+          .catch(error => {
+            // Handle error
+            console.log('Error:', error);
+          });
+      });
+    },[])
+  
+
+  
+  const url = `https://countriesnow.space/api/v0.1/countries/
+  `;
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await fetch(url);
         const data = await res.json();
+      
         setFilterdata(data.data);
       } catch (err) {
         console.log(err);
